b7b9c8552f4649f63b27224b74d5b781a956ac57
wizardy <wizardhassan99@gmail.com>
2024-12-30 14:06:29 +0100

    commit

---
+++ app/skills/page.jsx
@@ -0,0 +1,13 @@
+export default function Skills() {
+  return (
+    <div>
+      <h1>Mes Compétences</h1>
+      <ul>
+        <li>Développement web</li>
+        <li>React.js et Next.js</li>
+        <li>Node.js, Bun.js</li>
+        <li>Développement mobile (React Native)</li>
+      </ul>
+    </div>
+  );
+}
