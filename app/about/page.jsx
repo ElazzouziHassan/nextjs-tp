e7f1f80cc4d8b618643da3c1ee5dc8cfc5838eb3
wizardy <wizardhassan99@gmail.com>
2024-12-30 14:06:19 +0100

    commit

---
+++ app/about/page.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>À propos de moi</h1>
+      <p>Je suis un étudiant passionné par le développement web et les technologies modernes.</p>
+    </div>
+  );
+}
